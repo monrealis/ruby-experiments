449de57991fe6add3623e81288518ea3822920b4
Vytenis Bivainis <monrealis@gmail.com>
2015-02-08 23:19:48 +0200

    Remove deprecated checks.

--- rspec/List_spec.rb
+++ rspec/List_spec.rb
@@ -5,10 +5,10 @@ describe 'Contains method' do
   before do
     @list = [*1..3]
   end
-  it 'should return true if value is in list' do
+  it 'should return true if value is in the list' do
     expect(@list.include? 3).to eq(true)
   end
-  it 'should return true if value is in list' do
-    (@list.include? 4).should == false
+  it 'should return false if value is not in the list' do
+    expect(@list.include? 4).to eq(false)
   end
 end
