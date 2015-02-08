ca0ba6c70f01c4b22c396d4245ec3d470cc06c77
Vytenis Bivainis <monrealis@gmail.com>
2015-02-08 18:57:30 +0200

    Extract methods.

--- interesting-scripts/benford.rb
+++ interesting-scripts/benford.rb
@@ -1,13 +1,24 @@
 require 'net/http'
 require 'uri'
 
-uri = URI.parse('http://www.delfi.lt')
-response = Net::HTTP.get_response(uri)
-text = response.body
-counts = text.split('').grep('0'..'9').group_by { |c| c }.map { |char, chars| [char, chars.count] }
-total = counts.map { |_, v| v }.reduce { |i, j| i + j }
-counts.sort.each do |char, chars|
-  percent = 100.0 * chars/total
-  p = '%.2f' % percent
-  puts "#{char}: #{chars}/#{total} (#{p}%)"
+def get_text
+  uri = URI.parse('http://www.delfi.lt')
+  response = Net::HTTP.get_response(uri)
+  response.body
 end
+
+def get_counts(text)
+  text.split('').grep('0'..'9').group_by { |c| c }.map { |char, chars| [char, chars.count] }
+end
+
+def print_counts(counts)
+  total = counts.map { |_, v| v }.reduce { |i, j| i + j }
+  counts.sort.each do |char, chars|
+    percent = 100.0 * chars/total
+    p = '%.2f' % percent
+    puts "#{char}: #{chars}/#{total} (#{p}%)"
+  end
+end
+
+
+print_counts(get_counts(get_text))
