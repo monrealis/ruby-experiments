9e5383b58589eb57d46af354e2d252dfae438193
Vytenis Bivainis <monrealis@gmail.com>
2015-02-12 01:37:57 +0200

    Add XmlVisualizer class.

--- xml-visualizer/xml_visualizer.rb
+++ xml-visualizer/xml_visualizer.rb
@@ -6,5 +6,13 @@ module XmlVisLib
     def initialize(xml)
       @document = Document.new(xml)
     end
+
+    def visualize
+      elements = []
+      @document.children.each do |el|
+        elements << el.name
+      end
+      elements.join '\n'
+    end
   end
 end
--- xml-visualizer/xml_visualizer_spec.rb
+++ xml-visualizer/xml_visualizer_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative 'xml_visualizer'
 include XmlVisLib
 
-describe 'XML visualizer' do
+describe 'XML visualizer construction' do
   before do
     @xml = '<root />'
   end
@@ -11,12 +11,29 @@ describe 'XML visualizer' do
     parse
   end
 
-  def parse
-    XmlVisualizer.new @xml
-  end
-
   it 'should fail with invalid XML' do
     @xml = '<root>'
     expect { parse }.to raise_exception(ParseException)
   end
+end
+
+
+describe 'XML visualizer visualization' do
+  before do
+    @xml = '<root />'
+  end
+
+  it 'should return root element name if root is the only element' do
+    @xml = '<root />'
+    parse
+    expect(visualize).to eq('root')
+  end
+end
+
+def visualize
+  @visualizer.visualize
+end
+
+def parse
+  @visualizer = XmlVisualizer.new @xml
 end
